377528a4c80b433916f4fbca83b3db46ae1975e5
Rabie <rabiemoses24@gmail.com>
2024-02-02 15:56:16 +0800

    Connection to database established

--- djangodelights/djangodelights/settings.py
+++ djangodelights/djangodelights/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'djangodelights.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backend.postgresql',
-        'NAME': 'inventory',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
         'PASSWORD': 'wertyou1234',
-        'HOST': 'inventory.cpseucew2p7w.ap-southeast-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'HOST': 'inventory-project.cpseucew2p7w.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
